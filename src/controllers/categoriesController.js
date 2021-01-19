0bfdbfceac8400f348993de920263235704cd67b
thiagoribb <thiribeiro142@gmail.com>
2021-01-19 18:20:26 -0300

    feat: refactoring

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,5 +1,14 @@
 const Category = require("../models/Category");
 
 async function postCategory(name) {
-    awaitCategory.create({name});
+    await Category.create({name});
+}
+
+async function getCategories() {
+    await Category.create({name});
+}
+
+module.exports = { 
+    postCategory,
+    getCategories
 }
--- src/routers/categoriesRouters.js
+++ src/routers/categoriesRouters.js
@@ -6,10 +6,19 @@ const categoriesController = require("../controllers/categoriesController");
 router.post("/", async (req, res) => {
   try {
     await categoriesController.postCategory(req.body.name);
-    res.sendStatus(200);
+    res.sendStatus(201);
   } catch (err) {
     return res.sendStatus(500);
   }
 });
+
+router.get("/", async (req, res) => {
+    try {
+      const categories = await categoriesController.getCategories();
+      res.send(categories).status(200);
+    } catch (err) {
+      return res.sendStatus(500);
+    }
+  });
 
 module.exports = router;
