9da2a9e643d37f16e16c9565f7a64a51058b34f8
thiagoribb <thiribeiro142@gmail.com>
2021-01-22 11:37:58 -0300

    feat: remove unique cpf

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -3,10 +3,6 @@ const ForbiddenError = require("../errors/ForbiddenError");
 const InexistingIdError = require("../errors/InexistingIdError");
 
 async function postClient(clientData) {
-
-    const isThereThisCpf = await Client.findOne( { where: {cpf: clientData.cpf} } )
-    if(isThereThisCpf) throw new ForbiddenError();
-
     const client = await Client.create(clientData);
     return client;
 }
